336923198e5916cb159ec8f80d5565fedfbc8d3e
imano00 <nurimanjamal00@gmail.com>
2025-05-04 20:47:37 +0800

    add mock chart

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,9 +1,12 @@
 import { Calendar } from '@/components/ui/calendar';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { ChartConfig, ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart';
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import * as React from 'react';
+import { Bar, BarChart, CartesianGrid, XAxis } from 'recharts';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,21 +15,81 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+const chartData = [
+    { month: 'January', desktop: 186, mobile: 80 },
+    { month: 'February', desktop: 305, mobile: 200 },
+    { month: 'March', desktop: 237, mobile: 120 },
+    { month: 'April', desktop: 73, mobile: 190 },
+    { month: 'May', desktop: 209, mobile: 130 },
+    { month: 'June', desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+    desktop: {
+        label: 'Desktop',
+        color: '#2563eb',
+    },
+    mobile: {
+        label: 'Mobile',
+        color: '#60a5fa',
+    },
+} satisfies ChartConfig;
+
 export default function Dashboard() {
     const [date, setDate] = React.useState<Date | undefined>(new Date());
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="flex grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-auto flex-1 overflow-hidden rounded-xl border">
-                        <Calendar mode="single" selected={date} onSelect={setDate} className="rounded-md border" />
+                <div className="flex grid auto-rows-min gap-4 md:grid-cols-4">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-auto flex-auto overflow-hidden rounded-xl border outline-2">
+                        <Card className="align-center">
+                            <CardHeader>
+                                <CardTitle>Date</CardTitle>
+                                <CardDescription>
+                                    {date?.toLocaleDateString()} {date?.getHours()}
+                                    {':'}
+                                    {date?.getMinutes()}
+                                    {':'}
+                                    {date?.getSeconds()}
+                                </CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <Calendar mode="single" selected={date} onSelect={setDate} className="size-full w-fit w-full" />
+                            </CardContent>
+                        </Card>
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-auto flex-auto overflow-hidden rounded-xl border outline-2">
+                        <ChartContainer config={chartConfig} className="h-full min-h-[200px] w-full outline-2">
+                            <BarChart accessibilityLayer data={chartData}>
+                                <CartesianGrid vertical={false} />
+                                <XAxis
+                                    dataKey="month"
+                                    tickLine={false}
+                                    axisLine={false}
+                                    tickMargin={10}
+                                    tickFormatter={(value) => value.slice(0, 3)}
+                                />
+                                <ChartTooltip content={<ChartTooltipContent />} />
+                                <ChartLegend content={<ChartLegendContent />} />
+                                <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+                                <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+                            </BarChart>
+                        </ChartContainer>
+                    </div>
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-auto flex-auto overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
+                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-auto flex-auto overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
                     </div>
                 </div>
