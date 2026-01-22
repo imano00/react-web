9579d94bbae1b1fc8a3e407d7bfe4c643a0523da
imano00 <nurimanjamal00@gmail.com>
2026-01-22 22:14:37 +0800

    update inventory controller for index

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\InventoryItem;
 use Illuminate\Http\Request;
 
 class InventoryController extends Controller
 {
     public function index()
     {
-        return inertia('Inventory/Index');
+        $inventoryItems = InventoryItem::all(); // Fetch inventory items from the database
+        return inertia('Inventory/Index', [
+            'initialItems' => $inventoryItems
+        ]);
     }
 }
