ff254b808d74043595d141338789f5b3ebae20f4
imano00 <nurimanjamal00@gmail.com>
2026-02-02 21:41:02 +0800

    add new models and update inventoryitem

--- app/Models/InventoryItem.php
+++ app/Models/InventoryItem.php
@@ -4,19 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\SubCategory;
 
 class InventoryItem extends Model
 {
     use HasFactory;
     protected $fillable = [
         'name',
-        'category',
+        'subcategory_id',
         'unit',
         'current_stock',
         'reorder_level',
         'description',
     ];
 
+    public function subcategory()
+    {
+        return $this->belongsTo(SubCategory::class, 'subcategory_id');
+    }
     //Relationships
     public function stockMovements()
     {
---
+++ app/Models/SubCategory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubCategory extends Model
+{
+    protected $table = 'subcategories';
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function inventoryItems()
+    {
+        return $this->hasMany(InventoryItem::class);
+    }
+}
