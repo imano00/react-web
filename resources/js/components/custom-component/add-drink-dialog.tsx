f59a4834c2bae3e1543f1eb82fd2c282525bd5c9
imano00 <nurimanjamal00@gmail.com>
2026-02-12 23:14:09 +0800

    update drink dialog and separate drink form

--- resources/js/components/custom-component/add-drink-dialog.tsx
+++ resources/js/components/custom-component/add-drink-dialog.tsx
@@ -1,30 +1,29 @@
-import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
 import { useForm } from '@inertiajs/react';
 import React, { useState } from 'react';
 import { toast } from 'sonner';
-
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Textarea } from '../textarea';
 import { Button } from '../ui/button';
-import { Field, FieldGroup, FieldLabel, FieldSet } from '../ui/field';
-import { Input } from '../ui/input';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { DrinkForm } from './drink-form';
 
-export default function AddDrinkDialog() {
+type Subcategory = {
+    id: number;
+    name: string;
+    category: { id: number; name: string };
+};
+
+type Category = {
+    id: number;
+    name: string;
+    subcategories: Subcategory[];
+};
+
+export default function AddDrinkDialog({ categories }: { categories: Category[] }) {
     const [open, setOpen] = useState(false);
 
     const { data, setData, post, reset } = useForm({
         name: '',
-        category: '',
+        subcategory_id: undefined as number | undefined,
+        category_id: undefined as number | undefined,
         price: 0,
         description: '',
     });
@@ -42,68 +41,21 @@ export default function AddDrinkDialog() {
     };
 
     return (
-        <AlertDialog open={open} onOpenChange={setOpen}>
-            <AlertDialogTrigger asChild>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
                 <Button>+ Add New Drink</Button>
-            </AlertDialogTrigger>
+            </DialogTrigger>
 
-            <AlertDialogContent>
+            <DialogContent>
                 <form onSubmit={handleSubmit}>
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>Create New Drink 🧋</AlertDialogTitle>
-                        <AlertDialogDescription>Add a new drink item to your menu.</AlertDialogDescription>
-                    </AlertDialogHeader>
+                    <DialogHeader>
+                        <DialogTitle>Create New Drink 🧋</DialogTitle>
+                        <DialogDescription className="my-2">Add a new drink item to your menu.</DialogDescription>
+                    </DialogHeader>
 
-                    <FieldGroup>
-                        <FieldSet>
-                            <Field className="mt-4">
-                                <FieldLabel htmlFor="name">Item Name</FieldLabel>
-                                <Input id="name" value={data.name} onChange={(e) => setData('name', e.target.value)} />
-                            </Field>
-                            <Field>
-                                <FieldLabel htmlFor="category">Category</FieldLabel>
-                                <Select value={data.category} onValueChange={(v) => setData('category', v)}>
-                                    <SelectTrigger className="w-full">
-                                        <SelectValue placeholder="Select Category" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectGroup>
-                                            <SelectItem value="Coffee">Coffee</SelectItem>
-                                            <SelectItem value="Soda">Soda</SelectItem>
-                                            <SelectItem value="Chocolate">Chocolate</SelectItem>
-                                            <SelectItem value="Matcha">Matcha</SelectItem>
-                                        </SelectGroup>
-                                    </SelectContent>
-                                </Select>
-                            </Field>
-                            <Field>
-                                <FieldLabel htmlFor="price">Price (RM)</FieldLabel>
-                                <Input
-                                    id="price"
-                                    type="number"
-                                    step="1.00"
-                                    value={data.price}
-                                    onChange={(e) => setData('price', Number(e.target.value))}
-                                    className="w-full rounded-md border p-2"
-                                ></Input>
-                            </Field>
-                            <Field>
-                                <FieldLabel htmlFor="description">Description</FieldLabel>
-                                <Textarea
-                                    id="description"
-                                    value={data.description}
-                                    onChange={(e) => setData('description', e.target.value)}
-                                    className="w-full rounded-md border p-2"
-                                ></Textarea>
-                            </Field>
-                        </FieldSet>
-                    </FieldGroup>
-                    <AlertDialogFooter className="mt-4">
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <AlertDialogAction type="submit">Add Item</AlertDialogAction>
-                    </AlertDialogFooter>
+                    <DrinkForm data={data} setData={setData} categories={categories} mode="create" />
                 </form>
-            </AlertDialogContent>
-        </AlertDialog>
+            </DialogContent>
+        </Dialog>
     );
 }
