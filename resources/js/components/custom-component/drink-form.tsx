d47a45f12ea2f0f56b9499bc80a24a8f2b9c91c3
imano00 <nurimanjamal00@gmail.com>
2026-02-12 23:19:53 +0800

    update drink form

--- resources/js/components/custom-component/drink-form.tsx
+++ resources/js/components/custom-component/drink-form.tsx
@@ -2,6 +2,7 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 import { Button } from '../ui/button';
 import { Field, FieldGroup, FieldLabel, FieldSet } from '../ui/field';
 import { Input } from '../ui/input';
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput } from '../ui/input-group';
 import { Textarea } from '../ui/textarea';
 
 type Category = {
@@ -32,8 +33,17 @@ type DrinkFormProps = {
 };
 
 export function DrinkForm({ data, setData, categories = [], mode }: DrinkFormProps) {
-    const selectedCategory = categories.find((c) => c.id === data.category_id);
+    const drinkCategory = categories.find((c) => c.name === 'Drinks');
 
+    // const selectedCategory = categories.find((c) => c.id === data.category_id);
+
+    const incrementPrice = (amount: number) => {
+        setData('price', Number((Number(data.price ?? 0) + amount).toFixed(2)));
+    };
+
+    const decrementPrice = (amount: number) => {
+        setData('price', Number((Number(data.price ?? 0) - amount).toFixed(2)));
+    };
     return (
         <FieldGroup>
             <FieldSet>
@@ -52,6 +62,20 @@ export function DrinkForm({ data, setData, categories = [], mode }: DrinkFormPro
 
                 {/* Category */}
                 <Field>
+                    <FieldLabel>Category</FieldLabel>
+                    <Select value={drinkCategory?.id?.toString() ?? ''} disabled>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Select category" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectGroup>
+                                <SelectItem value={drinkCategory?.id?.toString() ?? ''}>Drinks</SelectItem>
+                            </SelectGroup>
+                        </SelectContent>
+                    </Select>
+                </Field>
+
+                {/* <Field>
                     <FieldLabel>Category</FieldLabel>
                     <Select
                         value={data.category_id?.toString() ?? ''}
@@ -73,7 +97,7 @@ export function DrinkForm({ data, setData, categories = [], mode }: DrinkFormPro
                             </SelectGroup>
                         </SelectContent>
                     </Select>
-                </Field>
+                </Field> */}
 
                 {/* Subcategory */}
                 <Field>
@@ -81,14 +105,14 @@ export function DrinkForm({ data, setData, categories = [], mode }: DrinkFormPro
                     <Select
                         value={data.subcategory_id?.toString() ?? ''}
                         onValueChange={(v) => setData('subcategory_id', Number(v))}
-                        disabled={!selectedCategory}
+                        disabled={!drinkCategory}
                     >
                         <SelectTrigger className="w-full">
                             <SelectValue placeholder="Select subcategory" />
                         </SelectTrigger>
                         <SelectContent>
                             <SelectGroup>
-                                {selectedCategory?.subcategories.map((sub) => (
+                                {drinkCategory?.subcategories.map((sub) => (
                                     <SelectItem key={sub.id} value={sub.id.toString()}>
                                         {sub.name.charAt(0).toUpperCase() + sub.name.slice(1)}
                                     </SelectItem>
@@ -100,8 +124,25 @@ export function DrinkForm({ data, setData, categories = [], mode }: DrinkFormPro
 
                 {/* Price */}
                 <Field>
-                    <FieldLabel>Price (RM)</FieldLabel>
-                    <Input type="number" value={data.price ?? ''} onChange={(e) => setData('price', Number(e.target.value))} />
+                    <FieldLabel htmlFor="price">Price (RM)</FieldLabel>
+                    <InputGroup>
+                        <InputGroupInput
+                            id="price"
+                            type="number"
+                            step="1.0"
+                            value={data.price}
+                            onChange={(e) => setData('price', Number(e.target.value))}
+                            className="w-full rounded-md p-2"
+                        />
+                        <InputGroupAddon align="inline-end">
+                            <InputGroupButton variant="outline" onClick={() => incrementPrice(1.0)}>
+                                +
+                            </InputGroupButton>
+                            <InputGroupButton variant="outline" onClick={() => decrementPrice(1.0)}>
+                                -
+                            </InputGroupButton>
+                        </InputGroupAddon>
+                    </InputGroup>
                 </Field>
 
                 {/* Description */}
